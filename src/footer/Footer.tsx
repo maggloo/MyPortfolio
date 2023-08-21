0f633c403392da533854bfa1165ff8b32d35d759
Margarita P <rprivalko@gmail.com>
2023-08-21 16:50:51 +0300

    fix: used social media buttons as a separate component

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import styles from './Footer.module.scss';
 import styleContainer from '../common/styles/Container.module.scss'
 import {Fade} from "react-awesome-reveal";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TelegramIcon from '@mui/icons-material/Telegram';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import {Icon, IconButton} from "@mui/material";
+import SocialMedia from "../common/components/social media/SocialMedia";
 
 
 const Footer = () => {
@@ -15,45 +11,7 @@ const Footer = () => {
             <div className={styles.footerBlock}>
                 <div className={`${styleContainer.container} ${styles.footerContainer}`}>
                     <h2 className={styles.title}>Margarita Privalko</h2>
-                    <div className={styles.socialsWrapper}>
-                        <div className={styles.social}>
-                            <IconButton
-                                href={'https://www.linkedin.com/in/margarita-privalko-6a6040240/'}
-                                target={'_blank'}
-                                sx={{
-                                    transition: '0.2s all',
-                                    '&:hover': {color: '#a75fe7', transition: '0.3s all'}
-                                }}
-                            >
-                                <LinkedInIcon/>
-                            </IconButton>
-                        </div>
-                        <div className={styles.social}>
-                            <IconButton
-                                href={'https://github.com/maggloo'}
-                                target={'_blank'}
-                                sx={{
-                                    transition: '0.2s all',
-                                    '&:hover': {color: '#a75fe7', transition: '0.3s all'}
-                                }}
-                            >
-                                <GitHubIcon/>
-
-                            </IconButton>
-                        </div>
-                        <div className={styles.social}>
-                            <IconButton
-                                href={'https://t.me/maggloo'}
-                                target={'_blank'}
-                                sx={{
-                                    transition: '0.2s all',
-                                    '&:hover': {color: '#a75fe7', transition: '0.3s all'}
-                                }}
-                            >
-                                <TelegramIcon/>
-                            </IconButton>
-                        </div>
-                    </div>
+                    <SocialMedia/>
                     <span>@ All Rights Reserved</span>
                 </div>
             </div>
